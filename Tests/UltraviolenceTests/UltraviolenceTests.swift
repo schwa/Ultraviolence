02164a02ca35c899523f807441e416da477ceb37
Jonathan Wight <schwa@schwa.io>
2024-10-31 09:49:20 -0700

    Add initial swiftlint.

--- Tests/UltraviolenceTests/UltraviolenceTests.swift
+++ Tests/UltraviolenceTests/UltraviolenceTests.swift
@@ -8,21 +8,21 @@ struct RenderingTests {
     func simpleRender() async throws {
         let source = """
         #include <metal_stdlib>
-    
+
         using namespace metal;
-    
+
         struct VertexIn {
             float4 position [[attribute(0)]];
         };
-    
+
         struct VertexOut {
             float4 position [[position]];
         };
-    
+
         [[vertex]] VertexOut vertex_main(const VertexIn in [[stage_in]]) {
             return VertexOut { in.position };
         }
-    
+
         [[fragment]] float4 fragment_main() {
             return float4(1.0, 0.0, 0.0, 1.0);
         }
