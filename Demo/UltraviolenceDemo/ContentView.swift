c83045390da71501474e25c8e15b76923cd91b5a
Jonathan Wight <jwight@mac.com>
2024-11-01 13:56:39 -0700

    Adjust parameters.

--- Demo/UltraviolenceDemo/ContentView.swift
+++ Demo/UltraviolenceDemo/ContentView.swift
@@ -57,7 +57,7 @@ struct TeapotRenderPass: RenderPass {
         
         // Rim lighting calculation
         float rim = pow(1.0 - dot(in.worldNormal, viewDir), 2.0);
-        float rimIntensity = 0.5 * rim;  // Adjust the intensity of the rim light as needed
+        float rimIntensity = 0.25 * rim;  // Adjust the intensity of the rim light as needed
         
         // Combine Lambertian shading and rim lighting
         float combinedIntensity = lambertian * rimIntensity;
@@ -100,7 +100,7 @@ struct ContentView: View {
     var angle: Angle = .zero
 
     @State
-    var camera: SIMD3<Float> = [0, 2, 4]
+    var camera: SIMD3<Float> = [0, 2, 6]
 
     var body: some View {
         TimelineView(.animation) { timeline in
@@ -113,7 +113,7 @@ struct ContentView: View {
             )
             .onGeometryChange(for: CGSize.self, of: \.size, action: { size = $0 })
             .onChange(of: timeline.date, initial: true) {
-                angle = Angle(degrees: (timeline.date.timeIntervalSince1970 * 400).truncatingRemainder(dividingBy: 360))
+                angle = Angle(degrees: (timeline.date.timeIntervalSince1970 * 120).truncatingRemainder(dividingBy: 360))
             }
         }
         .overlay(alignment: .topTrailing) {
