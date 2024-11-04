765a8ce5665313592c772c601aa07e884acbf339
Jonathan Wight <schwa@schwa.io>
2024-11-04 10:49:05 -0800

    Fix unit test.

--- Tests/UltraviolenceTests/UltraviolenceTests.swift
+++ Tests/UltraviolenceTests/UltraviolenceTests.swift
@@ -6,8 +6,10 @@ import Examples
 struct RenderingTests {
     @Test
     func simpleRender() async throws {
-        let renderer = Renderer(TeapotRenderPass(color: [1, 0, 0, 1], size: CGSize(width: 1600, height: 1200), model: .init(translation: [0, 0, 0]), view: .init(translation: [0, 0, 0]), cameraPosition: [0, 0, 0]))
-        let image = try renderer.render(size: CGSize(width: 1600, height: 1200)).cgImage
+        let size = CGSize(width: 1600, height: 1200)
+        let renderPass = TeapotRenderPass(color: [1, 0, 0, 1], size: size, model: .init(translation: [0, 0, 0]), view: .init(translation: [0, 0, 0]), cameraPosition: [0, 0, 0])
+        let renderer = Renderer(size: size, content: renderPass)
+        let image = try renderer.render().cgImage
         #expect(image != nil)
     }
 }
