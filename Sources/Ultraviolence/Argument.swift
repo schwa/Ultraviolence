bb0944b65cba61f000bf695c04f546d3b38f196d
Jonathan Wight <jwight@mac.com>
2024-11-05 20:34:50 -0800

    Re-org.

--- Sources/Ultraviolence/Argument.swift
+++ Sources/Ultraviolence/Argument.swift
@@ -1,5 +1,6 @@
 import Metal
 import simd
+internal import UltraviolenceSupport
 
 public struct Argument {
     public var functionType: MTLFunctionType
@@ -37,5 +38,63 @@ public extension RenderPass {
 
     func argument(type: MTLFunctionType, name: String, value: MTLTexture) -> some RenderPass {
         argument(type: type, name: name, value: .texture(value))
+    }
+}
+
+// MARK: -
+
+extension MTLRenderCommandEncoder {
+    func setArgument(_ argument: Argument, reflection: MTLRenderPipelineReflection) throws {
+        switch argument.functionType {
+        // TODO: Logic for .fragment and .vertex are almost identical.
+        case .fragment:
+            guard let binding = reflection.fragmentBindings.first(where: { $0.name == argument.name }) else {
+                throw UltraviolenceError.missingBinding(argument.name)
+            }
+            switch argument.value {
+            case .float3, .float4, .matrix4x4:
+                try withUnsafeBytes(of: argument.value) { buffer in
+                    let baseAddress = try buffer.baseAddress.orThrow(.resourceCreationFailure)
+                    setFragmentBytes(baseAddress, length: buffer.count, index: binding.index)
+                }
+            case .texture(let texture):
+                setFragmentTexture(texture, index: binding.index)
+            }
+        case .vertex:
+            guard let binding = reflection.vertexBindings.first(where: { $0.name == argument.name }) else {
+                throw UltraviolenceError.missingBinding(argument.name)
+            }
+            switch argument.value {
+            case .float3, .float4, .matrix4x4:
+                try withUnsafeBytes(of: argument.value) { buffer in
+                    let baseAddress = try buffer.baseAddress.orThrow(.resourceCreationFailure)
+                    setVertexBytes(baseAddress, length: buffer.count, index: binding.index)
+                }
+            case .texture(let texture):
+                setVertexTexture(texture, index: binding.index)
+            }
+        default:
+            fatalError("Not implemented")
+        }
+    }
+}
+
+// MARK: -
+
+extension MTLComputeCommandEncoder {
+    func setArgument(_ argument: Argument, reflection: MTLComputePipelineReflection) throws {
+        assert(argument.functionType == .kernel)
+        let binding = try reflection.bindings.first { $0.name == argument.name }.orThrow(.missingBinding("\(argument.name)"))
+        switch argument.value {
+        case .float3, .float4, .matrix4x4:
+            try withUnsafeBytes(of: argument.value) { buffer in
+                guard let baseAddress = buffer.baseAddress else {
+                    throw UltraviolenceError.resourceCreationFailure
+                }
+                setBytes(baseAddress, length: buffer.count, index: binding.index)
+            }
+        case .texture(let texture):
+            setTexture(texture, index: binding.index)
+        }
     }
 }
