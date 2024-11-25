4714215b2935b3e09465ada1e79e6f6794620dea
Jonathan Wight <jwight@mac.com>
2024-11-24 20:45:35 -0800

    Offscreen rendering seems working again.

--- Sources/UltraviolenceRedux/RenderPasses.swift
+++ Sources/UltraviolenceRedux/RenderPasses.swift
@@ -9,6 +9,7 @@ public extension EnvironmentValues {
     @Entry var renderPassDescriptor: MTLRenderPassDescriptor?
     @Entry var renderPipelineState: MTLRenderPipelineState?
     @Entry var vertexDescriptor: MTLVertexDescriptor?
+    @Entry var renderPipelineReflection: MTLRenderPipelineReflection?
 }
 
 // MARK: -
@@ -30,6 +31,35 @@ public struct FragmentShader {
         let device = MTLCreateSystemDefaultDevice()!
         let library = try device.makeLibrary(source: source, options: nil)
         function = library.functionNames.compactMap { library.makeFunction(name: $0) }.first { $0.functionType == .fragment }!
+    }
+}
+
+public extension VertexShader {
+    var vertexDescriptor: MTLVertexDescriptor? {
+        function.vertexDescriptor
+    }
+}
+
+extension MTLFunction {
+    var vertexDescriptor: MTLVertexDescriptor? {
+        guard let vertexAttributes else {
+            return nil
+        }
+        let vertexDescriptor = MTLVertexDescriptor()
+
+        var totalStride: Int = 0
+        for attribute in vertexAttributes {
+            switch attribute.attributeType {
+            case .float2:
+                vertexDescriptor.attributes[attribute.attributeIndex].format = .float2
+                vertexDescriptor.layouts[attribute.attributeIndex].stride = MemoryLayout<SIMD2<Float>>.stride
+                totalStride += MemoryLayout<SIMD2<Float>>.stride
+            default:
+                fatalError()
+            }
+        }
+        vertexDescriptor.layouts[0].stride = totalStride
+        return vertexDescriptor
     }
 }
 
@@ -58,15 +88,14 @@ public struct Render <Content>: RenderPass, BodylessRenderPass where Content: Re
     }
 
     func drawEnter() {
-        print("RENDER: drawEnter")
     }
 
     func drawExit() {
-        print("RENDER: drawExit")
     }
 }
 
-public struct RenderPipeline <Content>: RenderPass where Content: RenderPass {
+public struct RenderPipeline <Content>: BodylessRenderPass where Content: RenderPass {
+    public typealias Body = Never
     @Environment(\.device)
     var device
 
@@ -76,9 +105,18 @@ public struct RenderPipeline <Content>: RenderPass where Content: RenderPass {
     @Environment(\.vertexDescriptor)
     var vertexDescriptor
 
+    @Environment(\.renderCommandEncoder)
+    var renderCommandEncoder
+
     var vertexShader: VertexShader
     var fragmentShader: FragmentShader
     var content: Content
+
+    @State
+    var renderPipelineState: MTLRenderPipelineState?
+
+    @State
+    var reflection: MTLRenderPipelineReflection?
 
     public init(vertexShader: VertexShader, fragmentShader: FragmentShader, @RenderPassBuilder content: () -> Content) {
         self.vertexShader = vertexShader
@@ -86,24 +124,43 @@ public struct RenderPipeline <Content>: RenderPass where Content: RenderPass {
         self.content = content()
     }
 
-    public var body: some RenderPass {
-        // TODO: Move this to a onSetup()
+    func _expandNode(_ node: Node) {
+        // TODO: Move into BodylessRenderPass
+        guard let graph = node.graph else {
+            fatalError("Cannot build node tree without a graph.")
+        }
+        if node.children.isEmpty {
+            node.children.append(graph.makeNode())
+        }
+        content.expandNode(node.children[0])
+
         let renderPipelineDescriptor = MTLRenderPipelineDescriptor()
         renderPipelineDescriptor.vertexFunction = vertexShader.function
         renderPipelineDescriptor.fragmentFunction = fragmentShader.function
-        renderPipelineDescriptor.vertexDescriptor = vertexDescriptor!
+        renderPipelineDescriptor.vertexDescriptor = vertexDescriptor ?? vertexShader.vertexDescriptor!
         renderPipelineDescriptor.colorAttachments[0].pixelFormat = renderPassDescriptor!.colorAttachments[0].texture!.pixelFormat
         renderPipelineDescriptor.depthAttachmentPixelFormat = renderPassDescriptor!.depthAttachment!.texture!.pixelFormat
-        let renderPipelineState = try! device!.makeRenderPipelineState(descriptor: renderPipelineDescriptor)
-        return content.environment(\.renderPipelineState, renderPipelineState)
+        let (renderPipelineState, reflection) = try! device!.makeRenderPipelineState(descriptor: renderPipelineDescriptor, options: .bindingInfo)
+        self.renderPipelineState = renderPipelineState
+        self.reflection = reflection
+
+        node.environmentValues[keyPath: \.renderPipelineState] = renderPipelineState
+        node.environmentValues[keyPath: \.renderPipelineReflection] = reflection
+    }
+
+    func drawEnter() {
+        renderCommandEncoder!.setRenderPipelineState(renderPipelineState!)
+    }
+
+    func drawExit() {
     }
 }
 
 public struct Draw: RenderPass, BodylessRenderPass {
     public typealias Body = Never
 
-    @Environment(\.commandBuffer)
-    var commandBuffer
+    @Environment(\.renderCommandEncoder)
+    var renderCommandEncoder
 
     var encodeGeometry: (MTLRenderCommandEncoder) throws -> Void
 
@@ -116,10 +173,86 @@ public struct Draw: RenderPass, BodylessRenderPass {
 
     func drawEnter() {
         print("drawEnter")
-        print(commandBuffer)
+        try! encodeGeometry(renderCommandEncoder!)
     }
 
     func drawExit() {
         print("drawExit")
+    }
+}
+
+public extension RenderPass {
+    func parameter<T>(_ name: String, _ value: T) -> some RenderPass {
+        ParameterRenderPass(name: name, value: value, content: self)
+    }
+}
+
+struct ParameterRenderPass<Content, Value>: BodylessRenderPass where Content: RenderPass {
+    var name: String
+    var value: Value
+    var content: Content
+
+    @Environment(\.renderPipelineReflection)
+    var reflection
+
+    @Environment(\.renderCommandEncoder)
+    var renderCommandEncoder
+
+    func _expandNode(_ node: Node) {
+        // TODO: Move into BodylessRenderPass
+        guard let graph = node.graph else {
+            fatalError("Cannot build node tree without a graph.")
+        }
+        if node.children.isEmpty {
+            node.children.append(graph.makeNode())
+        }
+        content.expandNode(node.children[0])
+    }
+
+    func drawEnter() {
+        print("HERE")
+
+        let index = reflection!.binding(for: name)
+
+        withUnsafeBytes(of: value) { buffer in
+            renderCommandEncoder!.setFragmentBytes(buffer.baseAddress!, length: buffer.count, index: index)
+        }
+    }
+
+    func drawExit() {
+    }
+}
+
+// struct ModifiedRenderPass<Content, Modifier>: RenderPass where Content: RenderPass, Modifier: RenderPassModifier, Modifier.Content == Content {
+//    var content: Content
+//    var modifier: Modifier
+//
+//    var body: some RenderPass {
+//        modifier.body(content: content)
+//    }
+// }
+//
+// @MainActor public protocol RenderPassModifier {
+//    associatedtype Body : RenderPass
+//    @RenderPassBuilder @MainActor func body(content: Self.Content) -> Self.Body
+//    associatedtype Content
+// }
+//
+
+extension MTLRenderPipelineReflection {
+    func binding(for name: String) -> Int {
+        let indices = [vertexBindings.first { $0.name == name }?.index,
+        fragmentBindings.first { $0.name == name }?.index,
+        tileBindings.first { $0.name == name }?.index,
+        objectBindings.first { $0.name == name }?.index,
+        meshBindings.first { $0.name == name }?.index]
+        .compactMap { $0 }
+        if indices.isEmpty {
+            fatalError("No binding for \(name)")
+        }
+        else if indices.count > 1 {
+            fatalError("Ambiguous binding for \(name)")
+        }
+        return indices.first!
     }
 }
