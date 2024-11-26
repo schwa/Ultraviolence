0dcd6f62e0e5b2bfc88a0e730fa8bf4f6adc6398
Jonathan Wight <jwight@mac.com>
2024-11-25 17:34:53 -0800

    RenderView redux.

---
+++ Sources/UltraviolenceRedux/RenderView.swift
@@ -0,0 +1,138 @@
+import Metal
+import MetalKit
+internal import os
+import SwiftUI
+
+#if os(macOS)
+public struct RenderView <Content>: NSViewRepresentable where Content: RenderPass {
+    let device = MTLCreateSystemDefaultDevice()!
+    let content: (MTLRenderPassDescriptor) -> Content
+
+    public init(@RenderPassBuilder _ content: @escaping (MTLRenderPassDescriptor) -> Content) {
+        self.content = content
+    }
+
+    public func makeCoordinator() -> RenderPassCoordinator<Content> {
+        do {
+            return try .init(device: device, content: content)
+        } catch {
+            fatalError("Failed to create render pass coordinator: \(error)")
+        }
+    }
+
+    public func makeNSView(context: Context) -> MTKView {
+        let view = MTKView()
+        view.device = device
+        view.delegate = context.coordinator
+        // TODO: To be honest all of these settings should be configurable.
+        view.clearColor = MTLClearColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
+        view.colorPixelFormat = .bgra8Unorm_srgb
+        view.depthStencilPixelFormat = .depth32Float
+        view.depthStencilAttachmentTextureUsage = [.shaderRead, .renderTarget]
+        view.framebufferOnly = false
+        return view
+    }
+
+    public func updateNSView(_ nsView: MTKView, context: Context) {
+        context.coordinator.content = content
+    }
+}
+#elseif os(iOS)
+public struct RenderView <Content>: UIViewRepresentable where Content: RenderPass {
+    let device = MTLCreateSystemDefaultDevice()!
+    let content: (MTLRenderPassDescriptor) -> Content
+
+    public init(@RenderPassBuilder _ content: @escaping (MTLRenderPassDescriptor) -> Content) {
+        self.content = content
+    }
+
+    public func makeCoordinator() -> RenderPassCoordinator<Content> {
+        // swiftlint:disable:next force_try
+        try! .init(device: device, content: content)
+    }
+
+    public func makeUIView(context: Context) -> MTKView {
+        let view = MTKView()
+        view.device = device
+        view.delegate = context.coordinator
+        // TODO: To be honest all of these settings should be configurable.
+        view.clearColor = MTLClearColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
+        view.colorPixelFormat = .bgra8Unorm_srgb
+        view.depthStencilPixelFormat = .depth32Float
+        view.depthStencilAttachmentTextureUsage = [.shaderRead, .renderTarget] // TODO
+        view.framebufferOnly = false
+        return view
+    }
+
+    public func updateUIView(_ nsView: MTKView, context: Context) {
+        context.coordinator.content = content
+    }
+}
+#endif
+
+// MARK: -
+
+public class RenderPassCoordinator <Content>: NSObject, MTKViewDelegate where Content: RenderPass {
+    var device: MTLDevice
+    var commandQueue: MTLCommandQueue
+    var content: Content
+    var lastError: Error?
+    var logger: Logger? = Logger()
+
+    init(device: MTLDevice, content: Content) throws {
+        self.device = device
+        self.content = content
+        self.commandQueue = try device.makeCommandQueue().orThrow(.resourceCreationFailure)
+    }
+
+    public func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+        // This line intentionally left blank.
+    }
+
+    public func draw(in view: MTKView) {
+        do {
+            guard let currentDrawable = view.currentDrawable else {
+                logger?.warning("No current drawable")
+                return
+            }
+            defer {
+                currentDrawable.present()
+            }
+            try commandQueue.withCommandBuffer(label: "􀐛RenderView.Coordinator.commandBuffer", debugGroup: "􀯕RenderView.Coordinator.draw()") { commandBuffer in
+                let renderPassDescriptor = view.currentRenderPassDescriptor!
+
+                let renderEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor)!
+
+                let root = content
+                    .environment(\.renderPassDescriptor, renderPassDescriptor)
+                    .environment(\.device, device)
+                    .environment(\.commandQueue, commandQueue)
+                    .environment(\.renderCommandEncoder, renderEncoder)
+
+                let graph = Graph(content: root)
+        //        graph.dump()
+
+                graph.visit { _, node in
+                    if let renderPass = node.renderPass as? any BodylessRenderPass {
+                        renderPass._setup(node)
+                    }
+                }
+                enter: { node in
+                    if let body = node.renderPass as? any BodylessRenderPass {
+                        body.drawEnter()
+                    }
+                }
+                exit: { node in
+                    if let body = node.renderPass as? any BodylessRenderPass {
+                        body.drawExit()
+                    }
+                }
+                renderEncoder.endEncoding()
+                commandBuffer.commit()
+            }
+        } catch {
+            logger?.error("Error when drawing: \(error)")
+            lastError = error
+        }
+    }
+}
