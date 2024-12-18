9a8af3138d6510eb5307af55fa695ded6bab92ea
Jonathan Wight <jwight@mac.com>
2024-12-18 10:18:07 -0800

    Remove old Ultraviolence.

--- Sources/UltraviolenceOld/EmptyPass.swift
+++
@@ -1,9 +0,0 @@
-public struct EmptyPass: RenderPass {
-    public typealias Body = Never
-
-    public func visit(visitor: inout Visitor) throws {
-        try visitor.log(node: self) { _ in
-            // This line intentionally left blank.
-        }
-    }
-}
