c02d78aae7b046d1c30e3060b0159e980005aaf4
Jonathan Wight <jwight@mac.com>
2024-12-21 11:46:25 -0800

    Move Render().

--- Sources/Ultraviolence/RenderPasses.swift
+++ Sources/Ultraviolence/RenderPasses.swift
@@ -73,26 +73,6 @@ public extension VertexShader {
 
 // MARK: -
 
-// TODO: this should really be called renderpass
-public struct Render <Content>: RenderPass, BodylessRenderPass where Content: RenderPass {
-    var content: Content
-
-    public init(content: () -> Content) {
-        self.content = content()
-    }
-
-    func _expandNode(_ node: Node) throws {
-        // TODO: Move into BodylessRenderPass
-        guard let graph = node.graph else {
-            fatalError("Cannot build node tree without a graph.")
-        }
-        if node.children.isEmpty {
-            node.children.append(graph.makeNode())
-        }
-        try content.expandNode(node.children[0])
-    }
-}
-
 extension RenderPass {
     func _process(log: Bool = true) throws {
         let logger = log ? logger : nil
