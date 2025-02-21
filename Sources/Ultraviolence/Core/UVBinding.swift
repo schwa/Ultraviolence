0b92c1205535698fda01ea121d2fa94565cb1230
Jonathan Wight <jwight@mac.com>
2025-02-21 14:15:38 -0800

    Comment on UUID

--- Sources/Ultraviolence/Core/UVBinding.swift
+++ Sources/Ultraviolence/Core/UVBinding.swift
@@ -4,6 +4,7 @@ internal import Foundation
 public struct UVBinding<Value>: Equatable {
     private let get: () -> Value
     private let set: (Value) -> Void
+    // TOOD: Use a less expensive unique identifier
     private let id = UUID()
 
     public init(get: @escaping () -> Value, set: @escaping (Value) -> Void) {
