2bd2a268943d591208944dfc8ec56f244ee8a7a1
Jonathan Wight <jwight@mac.com>
2025-02-21 12:55:29 -0800

    Reorganise renderview

---
+++ Sources/Ultraviolence/RenderViewDebugViewModifier.swift
@@ -0,0 +1,37 @@
+import QuartzCore
+import SwiftUI
+import UltraviolenceSupport
+
+internal struct RenderViewDebugViewModifier <Root>: ViewModifier where Root: Element {
+    @State
+    var debugInspectorIsPresented = true
+
+    @Environment(RenderView<Root>.ViewModel.self)
+    var viewModel
+
+    func body(content: Content) -> some View {
+        content
+            .toolbar {
+                Toggle("Inspector", systemImage: "ladybug", isOn: $debugInspectorIsPresented)
+            }
+            .inspector(isPresented: $debugInspectorIsPresented) {
+                Text("\(viewModel.graph.root)")
+                List([NodeListBox(node: viewModel.graph.root)], children: \.children) { box in
+                    let node = box.node
+                    Text("\(node.shortDescription)").fixedSize().font(.caption2)
+                }
+                .inspectorColumnWidth(min: 200, ideal: 300)
+            }
+    }
+}
+
+internal struct NodeListBox: Identifiable {
+    var id: ObjectIdentifier {
+        ObjectIdentifier(node)
+    }
+    var node: Node
+    // swiftlint:disable:next discouraged_optional_collection
+    var children: [Self]? {
+        node.children.map { Self(node: $0) }
+    }
+}
