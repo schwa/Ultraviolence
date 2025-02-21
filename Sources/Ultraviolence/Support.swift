45b15d01d79107b52adaf97800348438fdad27e0
Jonathan Wight <jwight@mac.com>
2025-02-21 14:19:05 -0800

    Break out visitation code

--- Sources/Ultraviolence/Support.swift
+++ Sources/Ultraviolence/Support.swift
@@ -9,41 +9,6 @@ public struct EnvironmentDumper: Element, BodylessElement {
     }
 }
 
-extension Graph {
-    @MainActor
-    func visit(_ visitor: (Int, Node) throws -> Void, enter: (Node) throws -> Void = { _ in }, exit: (Node) throws -> Void = { _ in }) throws {
-        let saved = Graph.current
-        Graph.current = self
-        defer {
-            Graph.current = saved
-        }
-
-        try root.rebuildIfNeeded()
-
-        assert(activeNodeStack.isEmpty)
-
-        try root.visit(visitor) { node in
-            activeNodeStack.append(node)
-            try enter(node)
-        }
-        exit: { node in
-            try exit(node)
-            activeNodeStack.removeLast()
-        }
-    }
-}
-
-extension Node {
-    func visit(depth: Int = 0, _ visitor: (Int, Node) throws -> Void, enter: (Node) throws -> Void = { _ in }, exit: (Node) throws -> Void = { _ in }) rethrows {
-        try enter(self)
-        try visitor(depth, self)
-        try children.forEach { child in
-            try child.visit(depth: depth + 1, visitor, enter: enter, exit: exit)
-        }
-        try exit(self)
-    }
-}
-
 extension Element {
     var shortDescription: String {
         "\(type(of: self))"
---
+++ Sources/Ultraviolence/Visitation.swift
@@ -0,0 +1,34 @@
+extension Graph {
+    @MainActor
+    func visit(_ visitor: (Int, Node) throws -> Void, enter: (Node) throws -> Void = { _ in }, exit: (Node) throws -> Void = { _ in }) throws {
+        let saved = Graph.current
+        Graph.current = self
+        defer {
+            Graph.current = saved
+        }
+
+        try root.rebuildIfNeeded()
+
+        assert(activeNodeStack.isEmpty)
+
+        try root.visit(visitor) { node in
+            activeNodeStack.append(node)
+            try enter(node)
+        }
+        exit: { node in
+            try exit(node)
+            activeNodeStack.removeLast()
+        }
+    }
+}
+
+extension Node {
+    func visit(depth: Int = 0, _ visitor: (Int, Node) throws -> Void, enter: (Node) throws -> Void = { _ in }, exit: (Node) throws -> Void = { _ in }) rethrows {
+        try enter(self)
+        try visitor(depth, self)
+        try children.forEach { child in
+            try child.visit(depth: depth + 1, visitor, enter: enter, exit: exit)
+        }
+        try exit(self)
+    }
+}
