61019f62a153ec4714164f1d74d2e19650a9dd1c
Jonathan Wight <jwight@mac.com>
2025-02-21 20:46:44 -0800

    Use processor for Compute()

--- Sources/Ultraviolence/ComputePass+Compute.swift
+++ Sources/Ultraviolence/ComputePass+Compute.swift
@@ -6,30 +6,9 @@ public extension ComputePass {
     func compute() throws {
         // TODO: This has surprisingly little to do with compute. It's basically the same as offscreen rendererr.
         let device = try MTLCreateSystemDefaultDevice().orThrow(.resourceCreationFailure)
-        let commandBufferDescriptor = MTLCommandBufferDescriptor()
-        if logging {
-            try commandBufferDescriptor.addDefaultLogging()
-        }
         let commandQueue = try device.makeCommandQueue().orThrow(.resourceCreationFailure)
-        let commandBuffer = try commandQueue.makeCommandBuffer(descriptor: commandBufferDescriptor).orThrow(.resourceCreationFailure)
-        defer {
-            commandBuffer.commit()
-            commandBuffer.waitUntilCompleted()
-        }
-
+        let processor = Processor(device: device, completion: .commitAndWaitUntilCompleted, commandQueue: commandQueue)
         let content = self
-            .environment(\.device, device)
-
-        try commandBuffer.withDebugGroup("COMMAND BUFFER") {
-            var rootEnvironment = EnvironmentValues()
-            rootEnvironment.commandBuffer = commandBuffer
-            rootEnvironment.commandQueue = commandQueue
-
-            let content = content
-                .environment(\.commandBuffer, commandBuffer)
-                .environment(\.commandQueue, commandQueue)
-            let graph = try Graph(content: content)
-            try graph._process(rootEnvironment: rootEnvironment)
-        }
+        try processor.process(content)
     }
 }
