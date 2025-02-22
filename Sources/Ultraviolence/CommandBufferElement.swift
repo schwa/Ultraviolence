373421f387c242c04922b4cf0560d641e32a68a9
Jonathan Wight <jwight@mac.com>
2025-02-22 09:12:43 -0800

    Move into own files

---
+++ Sources/Ultraviolence/CommandBufferElement.swift
@@ -0,0 +1,33 @@
+import Metal
+import UltraviolenceSupport
+
+public struct CommandBufferElement <Content>: Element, FoobarElement where Content: Element {
+    var completion: MTLCommandQueueCompletion
+    var content: Content
+
+    init(completion: MTLCommandQueueCompletion, @ElementBuilder content: () throws -> Content) rethrows {
+        self.completion = completion
+        self.content = try content()
+    }
+
+    func _enter(_ node: Node, environment: inout UVEnvironmentValues) throws {
+        let device = try environment.device.orThrow(.missingEnvironment(\.commandBuffer))
+        let commandQueue = try environment.commandQueue.orThrow(.missingEnvironment(\.commandBuffer))
+        let commandBufferDescriptor = MTLCommandBufferDescriptor()
+        let commandBuffer = try commandQueue.makeCommandBuffer(descriptor: commandBufferDescriptor).orThrow(.resourceCreationFailure)
+        environment.commandBuffer = commandBuffer
+    }
+
+    func _exit(_ node: Node, environment: UVEnvironmentValues) throws {
+        let commandBuffer = try environment.commandBuffer.orThrow(.missingEnvironment(\.commandBuffer))
+        switch completion {
+        case .none:
+            break
+        case .commit:
+            commandBuffer.commit()
+        case .commitAndWaitUntilCompleted:
+            commandBuffer.commit()
+            commandBuffer.waitUntilCompleted()
+        }
+    }
+}
---
+++ Sources/Ultraviolence/FoobarElement.swift
@@ -0,0 +1,25 @@
+//
+//  FoobarElement.swift
+//  Ultraviolence
+//
+//  Created by Jonathan Wight on 2/22/25.
+//
+
+
+internal protocol FoobarElement: BodylessElement {
+    associatedtype Content: Element
+
+    var content: Content { get }
+}
+
+extension FoobarElement {
+    func _expandNode(_ node: Node, depth: Int) throws {
+        guard let graph = node.graph else {
+            preconditionFailure("Cannot build node tree without a graph.")
+        }
+        if node.children.isEmpty {
+            node.children.append(graph.makeNode())
+        }
+        try content.expandNode(node.children[0], depth: depth + 1)
+    }
+}
