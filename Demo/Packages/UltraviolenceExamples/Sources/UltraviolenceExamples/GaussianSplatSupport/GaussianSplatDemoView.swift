e3fe65b5877829221d41b8cdb006e16dbd001d70
Jonathan Wight <jwight@mac.com>
2025-03-19 10:17:08 -0700

    Add file picker and download buttons to splat

--- Demo/Packages/UltraviolenceExamples/Sources/UltraviolenceExamples/GaussianSplatSupport/GaussianSplatDemoView.swift
+++ Demo/Packages/UltraviolenceExamples/Sources/UltraviolenceExamples/GaussianSplatSupport/GaussianSplatDemoView.swift
@@ -39,6 +39,16 @@ public struct GaussianSplatDemoView: View {
             process(splats: items[0])
         })
         .toolbar {
+            SuperDownloadWidget("Download Splats…") { result in
+                if case let .success(url) = result {
+                    Task {
+                        splatCloud = try! await load(url: url)
+                    }
+                }
+            }
+            .environment(\.superdownloadWidgetBookmarks, [
+                URL(string: "https://huggingface.co/cakewalk/splat-data/resolve/main/train.splat")!
+            ])
             PopoverButton("Utilities", systemImage: "gear") {
                 Form {
                     Picker("Debug Mode", selection: $debugMode) {
@@ -138,53 +148,11 @@ public struct GaussianSplatView: View {
 extension GaussianSplatDemoView: DemoView {
 }
 
-internal struct SuperFilePickerModifier <T>: ViewModifier where T: Transferable & Sendable {
-    @State
-    private var isDropTargeted: Bool = false
+// MARK: -
 
-    @State
-    private var isFileImporterPresented: Bool = false
+// MARK: -
 
-    var callback: (Result<[T], Error>) throws -> Void
-
-    init(callback: @escaping (Result<[T], Error>) throws -> Void) {
-        self.callback = callback
-    }
-
-    func body(content: Content) -> some View {
-        content
-            .border(Color.blue, width: isDropTargeted ? 5 : 0)
-            .toolbar {
-                Button("Open…") {
-                    isFileImporterPresented = true
-                }
-            }
-            .dropDestination(for: T.self) { items, _ in
-                do {
-                    try callback(.success(items))
-                    return true
-                } catch {
-                    return false
-                }
-            }
-            isTargeted: { isTargeted in
-                isDropTargeted = isTargeted
-            }
-            .fileImporter(isPresented: $isFileImporterPresented, allowedContentTypes: T.importedContentTypes()) { result in
-                switch result {
-                case .success(let url):
-                    Task {
-                        do {
-                            let items = try await T(importing: url, contentType: nil)
-                            try callback(.success([items]))
-                        }
-                        catch {
-                            _ = try? callback(.failure(error))
-                        }
-                    }
-                case .failure(let error):
-                    _ = try? callback(.failure(error))
-                }
-            }
-    }
+extension EnvironmentValues {
+    @Entry
+    var superdownloadWidgetBookmarks: [URL] = []
 }
