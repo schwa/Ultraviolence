1513b1374b72f4be42870790d002ada64e9d8337
Jonathan Wight <jwight@mac.com>
2025-09-02 20:21:52 -0700

    Fix #104: Add non-optional overload for useResource
    - Added a non-optional version that takes a non-optional MTLResource
    - Optional version now calls the non-optional one when resource is present
    - Uses @ElementBuilder to handle the conditional logic properly

--- Sources/Ultraviolence/Support/Support.swift
+++ Sources/Ultraviolence/Support/Support.swift
@@ -22,14 +22,21 @@ internal extension Node {
 }
 
 public extension Element {
-    // TODO: #104 Not keen on this being optional.
     // TODO: #105 Need a compute variant of this.
-    func useResource(_ resource: (any MTLResource)?, usage: MTLResourceUsage, stages: MTLRenderStages) -> some Element {
+    func useResource(_ resource: any MTLResource, usage: MTLResourceUsage, stages: MTLRenderStages) -> some Element {
         onWorkloadEnter { environmentValues in
-            if let resource {
-                let renderCommandEncoder = environmentValues.renderCommandEncoder.orFatalError()
-                renderCommandEncoder.useResource(resource, usage: usage, stages: stages)
-            }
+            let renderCommandEncoder = environmentValues.renderCommandEncoder.orFatalError()
+            renderCommandEncoder.useResource(resource, usage: usage, stages: stages)
+        }
+    }
+    
+    @ElementBuilder
+    func useResource(_ resource: (any MTLResource)?, usage: MTLResourceUsage, stages: MTLRenderStages) -> some Element {
+        if let resource {
+            self.useResource(resource, usage: usage, stages: stages)
+        }
+        else {
+            self
         }
     }
 }
