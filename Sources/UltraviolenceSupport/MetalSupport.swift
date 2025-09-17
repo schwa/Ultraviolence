ce1d8b6863662233677f28ec8e60eb85e1101d60
Jonathan Wight <jwight@mac.com>
2025-09-17 10:51:01 -0700

    Refactored MTKMesh extensions
    - Moved MTKMesh utility methods into a separate file for better organisation.
    - Removed duplicate code by consolidating functions related to mesh creation and relabelling.
    - Defined a comprehensive `Options` struct to manage mesh generation options.
    - Enhanced error handling during mesh initialization for robustness.
    - Ensured Mesh vertex descriptors are set correctly for attribute consistency.

--- Sources/UltraviolenceSupport/MetalSupport.swift
+++ Sources/UltraviolenceSupport/MetalSupport.swift
@@ -12,54 +12,6 @@ import AppKit
 import UIKit
 #endif
 
-public extension MTKMesh {
-    convenience init(name: String, bundle: Bundle) throws {
-        let device = _MTLCreateSystemDefaultDevice()
-        let teapotURL = bundle.url(forResource: name, withExtension: "obj")
-        let vertexDescriptor = MDLVertexDescriptor()
-        vertexDescriptor.attributes[0] = MDLVertexAttribute(name: MDLVertexAttributePosition, format: .float3, offset: 0, bufferIndex: 0)
-        vertexDescriptor.attributes[1] = MDLVertexAttribute(name: MDLVertexAttributeNormal, format: .float3, offset: MemoryLayout<Float>.stride * 3, bufferIndex: 0)
-        vertexDescriptor.attributes[2] = MDLVertexAttribute(name: MDLVertexAttributeTextureCoordinate, format: .float2, offset: MemoryLayout<Float>.stride * 6, bufferIndex: 0)
-        vertexDescriptor.layouts[0] = MDLVertexBufferLayout(stride: MemoryLayout<Float>.stride * 8)
-        let mdlAsset = MDLAsset(url: teapotURL, vertexDescriptor: vertexDescriptor, bufferAllocator: MTKMeshBufferAllocator(device: device))
-        let mdlMesh = (mdlAsset.object(at: 0) as? MDLMesh).orFatalError(.resourceCreationFailure("Failed to load teapot mesh."))
-        try self.init(mesh: mdlMesh, device: device)
-    }
-
-    static func teapot() -> MTKMesh {
-        do {
-            return try MTKMesh(name: "teapot", bundle: .main)
-        }
-        catch {
-            fatalError("\(error)")
-        }
-    }
-
-    static func sphere(extent: SIMD3<Float> = [1, 1, 1], inwardNormals: Bool = false) -> MTKMesh {
-        do {
-            let device = _MTLCreateSystemDefaultDevice()
-            let allocator = MTKMeshBufferAllocator(device: device)
-            let mdlMesh = MDLMesh(sphereWithExtent: extent, segments: [48, 48], inwardNormals: inwardNormals, geometryType: .triangles, allocator: allocator)
-            return try MTKMesh(mesh: mdlMesh, device: device)
-        }
-        catch {
-            fatalError("\(error)")
-        }
-    }
-
-    static func plane(width: Float = 1, height: Float = 1, segments: SIMD2<UInt32> = [2, 2]) -> MTKMesh {
-        do {
-            let device = _MTLCreateSystemDefaultDevice()
-            let allocator = MTKMeshBufferAllocator(device: device)
-            let mdlMesh = MDLMesh(planeWithExtent: [width, height, 0], segments: segments, geometryType: .triangles, allocator: allocator)
-            return try MTKMesh(mesh: mdlMesh, device: device)
-        }
-        catch {
-            fatalError("\(error)")
-        }
-    }
-}
-
 public extension MTLDevice {
     func makeTexture(name: String, bundle: Bundle? = nil) throws -> MTLTexture {
         let textureLoader = MTKTextureLoader(device: self)
@@ -922,18 +874,6 @@ public extension MTLSamplerDescriptor {
     }
 }
 
-public extension MTKMesh {
-    func relabeled(_ label: String) -> MTKMesh {
-        for (index, buffer) in vertexBuffers.enumerated() {
-            buffer.buffer.label = "\(label)-vertexBuffer-\(index)"
-        }
-        for (index, submesh) in submeshes.enumerated() {
-            submesh.indexBuffer.buffer.label = "\(label)-submesh-indexBuffer-\(index)"
-        }
-        return self
-    }
-}
-
 public extension MTLBuffer {
     func gpuAddressAsUnsafeMutablePointer<T>(type: T.Type) -> UnsafeMutablePointer<T>? {
         precondition(MemoryLayout<Int>.stride == MemoryLayout<UInt64>.stride)
@@ -941,3 +881,4 @@ public extension MTLBuffer {
         return UnsafeMutablePointer<T>(bitPattern: bits)
     }
 }
+
